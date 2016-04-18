2d23b4a53b4be543fc776dd0d747f587278fe252
Sendu Bala <sb10@sanger.ac.uk>
2016-04-18 15:58:30 +0100

    jobqueue code split in to client.go and server.go, and partially implemented in a fast reliable way with Add() and Reserve() working so far on the client side, and Serve() working on the server side

---
+++ jobqueue/client.go
@@ -0,0 +1,278 @@
+// Copyright © 2016 Genome Research Limited
+// Author: Sendu Bala <sb10@sanger.ac.uk>.
+//
+//  This file is part of VRPipe.
+//
+//  VRPipe is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  VRPipe is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with VRPipe. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+Package jobqueue provides server/client functions to interact with the queue
+structure provided by the queue package over a network.
+
+This file contains all the functions for clients to interact with the server.
+See server.go for the functions needed to implement a server executable.
+*/
+package jobqueue
+
+import (
+	"github.com/go-mangos/mangos"
+	"github.com/go-mangos/mangos/protocol/req"
+	"github.com/go-mangos/mangos/transport/tcp"
+	"github.com/ugorji/go/codec"
+	"time"
+)
+
+// clientRequest is the struct that clients send to the server over the network
+// to request it do something. (The properties are only exported so the
+// encoder doesn't ignore them.)
+type clientRequest struct {
+	Method  string
+	Queue   string
+	Key     string
+	Jobs    []*Job
+	Timeout time.Duration
+}
+
+// Job is a struct that represents a command that needs to be run and some
+// associated metadata. CmdGroup is a string that you supply to group together
+// all commands that you expect to have similar memory and time requirements.
+// Memory and Time are added by the system based on past experience of running
+// jobs with the same CmdGroup. If you supply these yourself, your memory and
+// time will be used if there is insufficient past experience, or if you also
+// supply Override, which can be 0 to not override, 1 to override past
+// experience if your supplied values are higher, or 2 to always override.
+// Priority is a number between 0 and 255 inclusive - higher numbered jobs will
+// run before lower numbered ones (the default is 0). If you get a Job back
+// from the server (via Reserve() or Get()), you should treat the properties as
+// read-only: changing them will have no effect.
+type Job struct {
+	CmdGroup  string
+	Cmd       string
+	Cwd       string        // the working directory to cd to before running Cmd
+	Memory    int           // the expected peak memory in MB Cmd will use while running
+	Time      time.Duration // the expected time Cmd will take to run
+	Override  uint8
+	Priority  uint8
+	Peakmem   int           // the actual peak memory is recorded here (MB)
+	Exited    bool          // true if the Cmd was run and exited
+	Exitcode  int           // if the job ran and exited, its exit code is recorded here, but check Exited because when this is not set it could like like exit code 0
+	Pid       int           // the pid of the running or ran process is recorded here
+	Host      string        // the host the process is running or did run on is recorded here
+	Walltime  time.Duration // if the job ran or is running right now, the walltime for the run is recorded here
+	starttime time.Time     // the time the cmd starts running is recorded here
+	endtime   time.Time     // the time the cmd stops running is recorded here
+}
+
+// NewJob makes it a little easier to make a new Job, for use with Add()
+func NewJob(cmd string, cwd string, group string, memory int, time time.Duration, override uint8, priority uint8) *Job {
+	return &Job{
+		CmdGroup: group,
+		Cmd:      cmd,
+		Cwd:      cwd,
+		Memory:   memory,
+		Time:     time,
+		Override: override,
+		Priority: priority,
+	}
+}
+
+// client represents the client side of the socket that the jobqueue server is
+// Serve()ing, specific to a particular queue
+type client struct {
+	sock  mangos.Socket
+	queue string
+	ch    codec.Handle
+}
+
+// Connect creates a connection to the jobqueue server, specific to a single
+// queue. Note that if the server is not up, Connect() will not generate a
+// an error (you will just fail to achieve anything until the server starts).
+func Connect(addr string, queue string) (c *client, err error) {
+	sock, err := req.NewSocket()
+	if err != nil {
+		return
+	}
+
+	sock.AddTransport(tcp.NewTransport())
+
+	if err = sock.Dial(addr); err != nil {
+		return
+	}
+
+	c = &client{sock: sock, queue: queue, ch: new(codec.BincHandle)}
+	return
+}
+
+// Disconnect closes the connection to the jobqueue server
+func (c *client) Disconnect() {
+	c.sock.Close()
+}
+
+// Stats returns stats of the jobqueue server queue you connected to.
+// func (c *Conn) Stats() (s TubeStats, err error) {
+// 	data, err := c.beanstalk.StatsTube(c.tube)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to get stats for beanstalk tube %s: %s\n", c.tube, err.Error())
+// 		return
+// 	}
+// 	s = TubeStats{}
+// 	err = yaml.Unmarshal(data, &s)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to parse yaml for beanstalk tube %s stats: %s", c.tube, err.Error())
+// 	}
+// 	return
+// }
+
+// DaemonStats returns stats of the jobqueue server itself.
+// func (c *Conn) ServerStats() (s BeanstalkStats, err error) {
+// 	data, err := c.beanstalk.Stats()
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to get stats for beanstalkd: %s\n", err.Error())
+// 		return
+// 	}
+// 	s = BeanstalkStats{}
+// 	err = yaml.Unmarshal(data, &s)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to parse yaml for beanstalkd stats: %s\n", err.Error())
+// 	}
+// 	return
+// }
+
+// Add adds new jobs to the job queue, but only if those jobs aren't already in
+// there. If any where already there, you will not get an error, but the
+// returned 'existed' count will be > 0. Note that no cross-queue checking is
+// done, so you need to be careful not to add the same job to different queues.
+func (c *client) Add(jobs []*Job) (added int, existed int, err error) {
+	resp, err := c.request(&clientRequest{Method: "add", Queue: c.queue, Jobs: jobs})
+	if err != nil {
+		return
+	}
+	added = resp.Added
+	existed = resp.Existed
+	return
+}
+
+// Reserve takes a job off the jobqueue. If you process the job successfully you
+// should Delete() it. If you can't deal with it right now you should Release()
+// it. If you think it can never be dealt with you should Bury() it. If you die
+// unexpectedly, the job will automatically be released back to the queue after
+// some time. If no job was available in the queue for as long as the timeout
+// argument, nil is returned for both job and error. If your timeout is 0, you
+// will wait indefinitely for a job.
+func (c *client) Reserve(timeout time.Duration) (j *Job, err error) {
+	resp, err := c.request(&clientRequest{Method: "reserve", Queue: c.queue, Timeout: timeout})
+	if err != nil {
+		return
+	}
+	j = resp.Job
+	return
+}
+
+// Stats returns stats of a jobqueue job.
+// func (j *Job) Stats() (s JobStats, err error) {
+// 	data, err := j.conn.beanstalk.StatsJob(j.ID)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to get stats for beanstalk job %d: %s\n", j.ID, err.Error())
+// 		return
+// 	}
+// 	s = JobStats{}
+// 	err = yaml.Unmarshal(data, &s)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to parse yaml for beanstalk job %d stats: %s\n", j.ID, err.Error())
+// 	}
+// 	return
+// }
+
+// Delete removes a job from the jobqueue, for use after you have run the job
+// successfully.
+// func (j *Job) Delete() (err error) {
+// 	err = j.conn.beanstalk.Delete(j.ID)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to delete beanstalk job %d: %s\n", j.ID, err.Error())
+// 	}
+// 	return
+// }
+
+// Release places a job back on the jobqueue, for use when you can't handle the
+// job right now (eg. there was a suspected transient error) but maybe someone
+// else can later. Note that you must reserve a job before you can release it.
+// func (j *Job) Release() (err error) {
+// 	err = j.conn.beanstalk.Release(j.ID, 0, 60*time.Second)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to release beanstalk job %d: %s\n", j.ID, err.Error())
+// 	}
+// 	return
+// }
+
+// Touch resets a job's ttr, allowing you more time to work on it. Note that you
+// must reserve a job before you can touch it.
+// func (j *Job) Touch() (err error) {
+// 	err = j.conn.beanstalk.Touch(j.ID)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to touch beanstalk job %d: %s\n", j.ID, err.Error())
+// 	}
+// 	return
+// }
+
+// Bury marks a job as unrunnable, so it will be ignored (until the user does
+// something to perhaps make it runnable and kicks the job). Note that you must
+// reserve a job before you can bury it.
+// func (j *Job) Bury() (err error) {
+// 	err = j.conn.beanstalk.Bury(j.ID, 0)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to bury beanstalk job %d: %s\n", j.ID, err.Error())
+// 	}
+// 	return
+// }
+
+// Kick makes a previously Bury()'d job runnable again (it can be reserved in
+// the future).
+// func (j *Job) Kick() (err error) {
+// 	err = j.conn.beanstalk.KickJob(j.ID)
+// 	if err != nil {
+// 		err = fmt.Errorf("Failed to kick beanstalk job %d: %s\n", j.ID, err.Error())
+// 	}
+// 	return
+// }
+
+// request the server do something and get back its response
+func (c *client) request(cr *clientRequest) (sr *serverResponse, err error) {
+	// encode and send the request
+	var encoded []byte
+	enc := codec.NewEncoderBytes(&encoded, c.ch)
+	err = enc.Encode(cr)
+	if err != nil {
+		return
+	}
+	err = c.sock.Send(encoded)
+	if err != nil {
+		return
+	}
+
+	// get the response and decode it
+	resp, err := c.sock.Recv()
+	if err != nil {
+		return
+	}
+	sr = &serverResponse{}
+	dec := codec.NewDecoderBytes(resp, c.ch)
+	err = dec.Decode(sr)
+	if err != nil {
+		return
+	}
+
+	// pull the error out of sr
+	err = sr.Err
+	return
+}
