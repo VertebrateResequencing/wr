f237656a1ae8c4b37157a106bea8133c4d72421f
Sendu Bala <sb10@sanger.ac.uk>
2016-12-04 21:31:15 +0000

    now we make our own ssh private/public key pair, since latest openstack does not return DER format keys

--- cloud/openstack.go
+++ cloud/openstack.go
@@ -21,6 +21,10 @@ package cloud
 // This file contains a provideri implementation for OpenStack
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
@@ -35,6 +39,7 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/networks"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/subnets"
 	"github.com/gophercloud/gophercloud/pagination"
+	"golang.org/x/crypto/ssh"
 	"os"
 	"regexp"
 	"strings"
@@ -145,14 +150,29 @@ func (p *openstackp) deploy(resources *Resources, requiredPorts []int) (err erro
 	kp, err := keypairs.Get(p.computeClient, resources.ResourceName).Extract()
 	if err != nil {
 		if _, notfound := err.(gophercloud.ErrDefault404); notfound {
-			// create a new keypair; if we don't supply our own public key, it
-			// makes a private key for us
-			kp, err = keypairs.Create(p.computeClient, keypairs.CreateOpts{Name: resources.ResourceName}).Extract()
+			// create a new keypair; we can't just let Openstack create one for
+			// us because in latest versions it does not return a DER encoded
+			// key, which is what GO built-in library supports.
+			privateKey, errk := rsa.GenerateKey(rand.Reader, 1024)
+			if errk != nil {
+				err = errk
+				return
+			}
+			privateKeyPEM := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)}
+			privateKeyPEMBytes := pem.EncodeToMemory(privateKeyPEM)
+			pub, errk := ssh.NewPublicKey(&privateKey.PublicKey)
+			if errk != nil {
+				err = errk
+				return err
+			}
+			publicKeyStr := ssh.MarshalAuthorizedKey(pub)
+
+			kp, err = keypairs.Create(p.computeClient, keypairs.CreateOpts{Name: resources.ResourceName, PublicKey: string(publicKeyStr)}).Extract()
 			if err != nil {
 				return
 			}
 
-			resources.PrivateKey = kp.PrivateKey
+			resources.PrivateKey = string(privateKeyPEMBytes)
 		} else {
 			return
 		}
