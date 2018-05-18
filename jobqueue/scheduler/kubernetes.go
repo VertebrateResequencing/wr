04fffd0a4f62fe11fac841fe141b6e2fbd0e5a15
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-05-18 15:47:46 +0100

    Wireframe implementation

--- jobqueue/scheduler/kubernetes.go
+++ jobqueue/scheduler/kubernetes.go
@@ -19,21 +19,105 @@
 package scheduler
 
 import (
+	"github.com/VertebrateResequencing/wr/cloud"
+	"github.com/VertebrateResequencing/wr/kubernetes/client"
+	kubescheduler "github.com/VertebrateResequencing/wr/kubernetes/scheduler"
 	"github.com/inconshreveable/log15"
+	kubeinformers "k8s.io/client-go/informers"
+	"time"
 )
 
-//Kubernetes is the implementer of scheduleri. Modeled after the opst implementation
-//in openstack.go
+// Kubernetes is the implementer of scheduleri.
+// it is a wrapper to implement scheduleri by sending requests to the controller.
+// all of the logic to determine how / if a cmd can be scheduled is taken care of inside the controller.
+// this reduces communication between the two pieces of code.
+
+// maxQueueTime(), reserveTimeout(), hostToID()
+// are inherited from local
 type k8s struct {
 	local
 	log15.Logger
+	callBackChan chan string
+	requestChan  chan request
 }
 
+type request struct {
+	job job
+	ack chan struct{}
+	err error
+}
+
+// Set up prerequisites, call Run()
+// Create channels to pass requests to the controller.
+func (s *k8s) initialize(namespace string, logger log15.Logger) error {
+	s.Logger = logger.New("scheduler", "kubernetes")
+	// Set up message notifier
+	s.callBackChan = make(chan string, 5)
+	go notifyMessage(s.callBackChan)
+	// Prerequisites to start the controller
+	libclient := &client.Kubernetesp{}
+	kubeClient, restConfig, err := libclient.Authenticate() // Authenticate against the cluster.
+	if err != nil {
+		return err
+	}
+	// Initialise all internal clients on  the provided namespace
+	libclient.Initialize(kubeClient, namespace)
+	// Initialise the informer factory
+	kubeInformerFactory := kubeinformers.NewFilteredSharedInformerFactory(kubeClient, time.Second*30, namespace)
+
+	// Create the controller
+	controller := kubescheduler.NewController(kubeClient, restConfig, libclient, kubeInformerFactory)
+
+	go kubeInformerFactory.Start(stopCh)
+
+	// Start the scheduling controller
+	if err = controller.Run(2, stopCh); err != nil {
+		logger.Error("Error running controller", err.Error())
+	}
+
+	return nil
+}
+
+// creates a schedule request to send to the controller.
 func (s *k8s) schedule(cmd string, req *Requirements, count int) error {
 	return nil
 }
 
-//Delete the namespace when all pods have exited.
+// creates a busy request to send to the controller.
+func (s *k8s) busy() bool {
+	return nil
+}
+
+// setMessageCallBack sets the given callback.
+func (s *k8s) setMessageCallback(cb MessageCallBack) {
+	s.cbmutex.Lock()
+	defer s.cbmutex.Unlock()
+	s.msgCB = cb
+}
+
+// setBadServerCallBack sets the given callback.
+func (s *k8s) setBadServerCallBack(cb BadServerCallBack) {
+	s.cbmutex.Lock()
+	defer s.cbmutex.Unlock()
+	s.badServerCB = cb
+}
+
+// The controller is passed a callback channel.
+// notifyMessage recieves on the channel
+// if anything is recieved call s.msgCB(msg).
+func (s *k8s) notifyCallBack(callBackChan chan string, badCallBackChan chan *cloud.Server) {
+	for {
+		select {
+		case msg := <-callBackChan:
+			go s.msgCB(msg)
+		case badServer := <-badCallBackChan:
+			go s.badServerCB(badServer)
+		}
+	}
+
+}
+
+// Delete the namespace when all pods have exited.
 func (s *k8s) cleanup() {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
@@ -48,22 +132,4 @@ func (s *k8s) cleanup() {
 	//TODO: Do more elegantly later.
 
 	return
-}
-
-//Always assume the cluster can schedule the job.
-//ToDo: Check if any given node has the required ram / cpu
-func (s *k8s) reqCheck(req *Requirements) error {
-	return nil
-}
-
-//Arbitrarily limit max jobs to 50 for now
-//ToDo: Use input from kubectl describe nodes under allocatable
-func (s *k8s) canCount(req *Requirements) int {
-	return 50
-}
-
-//Should create a new pod here. Unsure of how to structure
-//Want to use implementation from local, just on a pod running the runner.
-func (s *k8s) runCmd(cmd string, req *Requirements, reservedCh chan bool) error {
-	return nil
 }
