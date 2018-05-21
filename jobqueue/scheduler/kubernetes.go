3583032038c3c2678509d3bd0d9209d4191e0c14
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-05-21 15:30:47 +0100

    Move queue creation to initialize()

--- jobqueue/scheduler/kubernetes.go
+++ jobqueue/scheduler/kubernetes.go
@@ -19,72 +19,97 @@
 package scheduler
 
 import (
+	"sync"
+	"time"
+
 	"github.com/VertebrateResequencing/wr/cloud"
 	"github.com/VertebrateResequencing/wr/kubernetes/client"
 	kubescheduler "github.com/VertebrateResequencing/wr/kubernetes/scheduler"
+	"github.com/VertebrateResequencing/wr/queue"
 	"github.com/inconshreveable/log15"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeinformers "k8s.io/client-go/informers"
-	"time"
 )
 
-// Kubernetes is the implementer of scheduleri.
-// it is a wrapper to implement scheduleri by sending requests to the controller.
-// all of the logic to determine how / if a cmd can be scheduled is taken care of inside the controller.
-// this reduces communication between the two pieces of code.
+// k8s is the implementer of scheduleri.
+// it is a wrapper to implement scheduleri by sending requests to the controller
 
-// maxQueueTime(), reserveTimeout(), hostToID()
+// maxQueueTime(), reserveTimeout(), hostToID(), busy(), schedule()
 // are inherited from local
 type k8s struct {
 	local
 	log15.Logger
-	callBackChan chan string
-	requestChan  chan request
-}
-
-type request struct {
-	job job
-	ack chan struct{}
-	err error
+	libclient       *client.Kubernetesp
+	callBackChan    chan string
+	cbmutex         sync.RWMutex
+	badCallBackChan chan *cloud.Server
+	reqChan         chan *Requirements
+	msgCB           MessageCallBack
+	badServerCB     BadServerCallBack
 }
 
 // Set up prerequisites, call Run()
 // Create channels to pass requests to the controller.
+// Create queue.
 func (s *k8s) initialize(namespace string, logger log15.Logger) error {
 	s.Logger = logger.New("scheduler", "kubernetes")
+
+	// make queue
+	s.queue = queue.New(localPlace)
+
+	// set our functions for use in schedule() and processQueue()
+	s.reqCheckFunc = s.reqCheck
+	s.canCountFunc = s.canCount
+	s.runCmdFunc = s.runCmd
+	s.cancelRunCmdFunc = s.cancelRun
+	s.stateUpdateFunc = s.stateUpdate
+	s.stateUpdateFreq = s.config.StateUpdateFrequency
+	if s.stateUpdateFreq == 0 {
+		s.stateUpdateFreq = 1 * time.Minute
+	}
+
 	// Set up message notifier
 	s.callBackChan = make(chan string, 5)
-	go notifyMessage(s.callBackChan)
+	s.badCallBackChan = make(chan *cloud.Server, 5)
+	go s.notifyCallBack(s.callBackChan, s.badCallBackChan)
+
 	// Prerequisites to start the controller
-	libclient := &client.Kubernetesp{}
-	kubeClient, restConfig, err := libclient.Authenticate() // Authenticate against the cluster.
+	s.libclient = &client.Kubernetesp{}
+	kubeClient, restConfig, err := s.libclient.Authenticate() // Authenticate against the cluster.
 	if err != nil {
 		return err
 	}
+
 	// Initialise all internal clients on  the provided namespace
-	libclient.Initialize(kubeClient, namespace)
+	s.libclient.Initialize(kubeClient, namespace)
+
 	// Initialise the informer factory
-	kubeInformerFactory := kubeinformers.NewFilteredSharedInformerFactory(kubeClient, time.Second*30, namespace)
+	// Confine all informers to the provided namespace
+	kubeInformerFactory := kubeinformers.NewFilteredSharedInformerFactory(kubeClient, time.Second*30, namespace, func(listopts *metav1.ListOptions){
+		listopts.IncludeUninitialized = true
+		listopts.Watch = true
+	})
 
 	// Create the controller
-	controller := kubescheduler.NewController(kubeClient, restConfig, libclient, kubeInformerFactory)
+	controller := kubescheduler.NewController(kubeClient, restConfig, s.libclient, kubeInformerFactory)
+
+	stopCh := make(chan struct{})
 
 	go kubeInformerFactory.Start(stopCh)
 
 	// Start the scheduling controller
-	if err = controller.Run(2, stopCh); err != nil {
-		logger.Error("Error running controller", err.Error())
-	}
+	go func(stopCh) {
+		if err = controller.Run(2, stopCh); err != nil {
+			logger.Error("Error running controller", err.Error())
+		}
+	}(stopCh)
 
 	return nil
 }
 
-// creates a schedule request to send to the controller.
-func (s *k8s) schedule(cmd string, req *Requirements, count int) error {
-	return nil
-}
-
-// creates a busy request to send to the controller.
-func (s *k8s) busy() bool {
+// Send a request to see if a cmd with the provided requirements
+// can be scheduled.
+func (s *k8s) reqCheck(req *Requirements) error {
 	return nil
 }
 
@@ -128,8 +153,9 @@ func (s *k8s) cleanup() {
 		s.Warn("cleanup queue destruction failed", "err", err)
 	}
 
-	//TODO: Client-go code to delete namespace.
-	//TODO: Do more elegantly later.
-
+	err = s.libclient.TearDown()
+	if err != nil {
+		s.Warn("namespace deletion errored", "err", err)
+	}
 	return
 }
