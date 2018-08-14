8dd51455f58aedff3ec9a46207ace0ac849fb3ec
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-08-14 12:39:09 +0100

    Split unit/integration tests from e2e testse

--- kubernetes/run-e2e.sh
+++ kubernetes/run-e2e.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-
 echo -e '- Staring wr Kubernetes End-to-End tests'
-
 # Environment variables.
 echo -e '- Setting up the test environment..'
-
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 export KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
 
@@ -15,23 +12,7 @@ echo -e 'kubernetes version:\t' $(kubectl version -o json | jq .serverVersion.gi
 echo -e 'wr version:\t' $(git rev-parse --verify HEAD)
 echo ''
 
-# For more granularty test the 3 main parts work in isolation.
-
-# Test deployment controller.
-echo '- Testing wr kubernetes deployment:'
-
-# Run Go tests.
-echo '* Running client tests'
-GOCACHE=off go test -v ${SCRIPT_ROOT}/kubernetes/client/...
-
-echo '* Running deployment controller tests'
-GOCACHE=off go test -v ${SCRIPT_ROOT}/kubernetes/deployment/...
-
-echo '* Running scheduler controller tests'
-GOCACHE=off go test -v ${SCRIPT_ROOT}/kubernetes/scheduler/...
-
 # Test the compiled binary as the end user would run it. 
-
 # Test kubeDeployCmd
 /tmp/wr kubernetes deploy --debug || (cat ~/.wr_development/kubelog; /bin/false)
 
@@ -47,12 +28,8 @@ echo {42,24,mice,test} | xargs -n 1  echo echo | /tmp/wr add
 echo 'curl http://ovh.net/files/1Mio.dat -o /tmp/1Mio.dat' | /tmp/wr add --cloud_script /tmp/curl.sh --rtimeout 10
 
 # Test different can support the runner deployment method
-echo 'echo golang:latest' | /tmp/wr add --cloud_os golang:latest --rtimeout 100
-echo 'echo genomicpariscentre/samtools' | /tmp/wr add --cloud_os genomicpariscentre/samtools --rtimeout 100
-
-
-
-
+echo 'echo golang:latest' | /tmp/wr add --cloud_os golang:latest --rtimeout 10
+echo 'echo genomicpariscentre/samtools' | /tmp/wr add --cloud_os genomicpariscentre/samtools --rtimeout 10
 
 echo '* Running e2e tests'
 GOCACHE=off go test -v -timeout 500s ${SCRIPT_ROOT}/kubernetes/e2e/...
