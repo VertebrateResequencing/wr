4bd994f22cb4da87c1231ed3e215c7b41efc4816
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-08-16 11:41:57 +0100

    Add pending cluster tests

--- kubernetes/run-e2e.sh
+++ kubernetes/run-e2e.sh
@@ -36,7 +36,17 @@ echo 'echo golang:latest' | /tmp/wr add --cloud_os golang:latest --rtimeout 250
 echo 'echo genomicpariscentre/samtools' | /tmp/wr add --cloud_os genomicpariscentre/samtools --rtimeout 250
 
 echo '* Running e2e tests'
-GOCACHE=off go test -v -timeout 500s ${SCRIPT_ROOT}/kubernetes/e2e/...
+GOCACHE=off go test -v -timeout 500s ${SCRIPT_ROOT}/kubernetes/e2e/add_test
+
+# This should submit jobs that fit the entire node for each node in the cluster.
+# Submit twice to test jobs go from pending -> complete. 
+# Set rtimeout so that they pend for an amount of time
+kubectl get nodes -o json | jq -c -r '.items[] | .status | {cmd: " echo \(.addresses[] | select(.type=="Hostname")| .address)", cpus: (((.capacity.cpu | tonumber)*10)-5), rtimeout: 2 }'  | /tmp/wr add -i max \
+&& kubectl get nodes -o json | jq -c -r '.items[] | .status | {cmd: " echo \(.addresses[] | select(.type=="InternalIP")| .address)", cpus: (((.capacity.cpu | tonumber)*10)-5), rtimeout: 2 }'  | /tmp/wr add i- max
+
+echo '* Running node capacity e2e test'
+GOCACHE=off go test -v -timeout 500s ${SCRIPT_ROOT}/kubernetes/e2e/max_cluster
+
 
 # Test kubeTeardownCmd
 /tmp/wr kubernetes teardown || (cat ~/.wr_development/kubeScheduler{,Controller}log; /bin/false)
