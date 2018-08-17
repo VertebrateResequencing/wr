d924c42b227598c26a1da3b3db273267bbb11366
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-08-17 11:00:43 +0100

    remove extraneous error check

--- kubernetes/deployment/controller.go
+++ kubernetes/deployment/controller.go
@@ -279,12 +279,13 @@ func (c *Controller) processPod(obj *apiv1.Pod) {
 				c.Opts.Logger.Error(fmt.Sprintf("Failed to close resource file2: %s", err))
 			}
 
-			if err != nil {
-				c.Opts.Logger.Error(fmt.Sprintf("Failed to close resource file %s", err))
-			}
 			c.Opts.Logger.Info(fmt.Sprintf("Stored manager pod name %s in resource file", obj.ObjectMeta.Name))
 			c.Opts.Logger.Info(fmt.Sprintf("WR manager container is running, calling PortForward with ports %v", c.Opts.RequiredPorts))
-			go c.Client.PortForward(obj, c.Opts.RequiredPorts)
+			go func() {
+				err := c.Client.PortForward(obj, c.Opts.RequiredPorts)
+				if err != nil {
+				}
+			}()
 		default:
 			c.Opts.Logger.Debug(fmt.Sprintf("Not InitContainer or WR Manager container related"))
 		}
