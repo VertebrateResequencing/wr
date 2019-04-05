f2196e287cc0abc0af08a9a22f6a08ef4483fc7c
Sendu Bala <sb10@sanger.ac.uk>
2019-04-05 10:36:25 +0100

    Do not repeat exports in CreateSharedDisk().

--- cloud/server.go
+++ cloud/server.go
@@ -21,6 +21,7 @@ package cloud
 // This file contains the code for the Server struct.
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"errors"
@@ -770,10 +771,25 @@ func (s *Server) CreateSharedDisk() error {
 		return err
 	}
 
-	cmd = exec.CommandContext(ctx, "bash", "-c", fmt.Sprintf("echo '%s *(rw,sync,no_root_squash)' | sudo tee --append /etc/exports > /dev/null", sharePath)) // #nosec
-	err = cmd.Run()
+	f, err := os.Open("/etc/exports")
 	if err != nil {
 		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var found bool
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), sharePath) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		cmd = exec.CommandContext(ctx, "bash", "-c", fmt.Sprintf("echo '%s *(rw,sync,no_root_squash)' | sudo tee --append /etc/exports > /dev/null", sharePath)) // #nosec
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
 	}
 
 	if _, errs := os.Stat(sharePath); errs != nil && os.IsNotExist(errs) {
